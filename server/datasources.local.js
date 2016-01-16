5f18a8b860859ef13b905fb89b398488918d7a23
Gabriel Martinez <gmartinez@itexico.net>
2016-01-15 18:05:14 -0600

    new files

---
+++ server/datasources.local.js
@@ -0,0 +1,9 @@
+// messenger-api/server/datasources.local.js
+module.exports = {
+    // DB Datasource
+    db: {
+        name: 'db',
+        connector: 'mongodb',
+        url: 'mongodb://reverUser:Rev3r@127.0.0.1/rever'
+    }
+};
