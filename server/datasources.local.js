cb171ddd9c666553ac8709bc0b6d71eca84828e7
Gabriel Martinez <gmartinez@itexico.net>
2016-01-19 12:28:52 -0600

    configuration to upload and download files using component loopbackk component

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -5,5 +5,11 @@ module.exports = {
         name: 'db',
         connector: 'mongodb',
         url: 'mongodb://reverUser:Rev3r@127.0.0.1/rever'
-    }
+    },
+  	storage: {
+    	name: "storage",
+    	connector: "loopback-component-storage",
+    	provider: 'filesystem',
+    	root: './server/storage'
+  	}
 };
